3372e2cde145892c1b0e389b4c2e5adcbcc0f080
Aline <alinecaleffi97@gmail.com>
2022-05-12 17:57:21 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
+import Listing from "pages/Listing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Form from 'pages/Form';
 
 function App() {
   return (
-    <Navbar></Navbar>
+    // BrowserRouter é um componente responsável por informar a nossa aplicação 
+    // que teremos um roteamento de componentes
+    <BrowserRouter>
+      <Navbar />
+      {/* Routes: componente que associa a rota ao componente.
+          Nele temos três parâmetros: component, path e exact.*/}
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
